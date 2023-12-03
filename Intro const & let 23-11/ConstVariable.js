262e4a3bedd71fc0e82ed21036d4af8f6f15028a
Sambhaji Shivaji Pandhare <sambhaji.pandhare@in.techwave.net>
2023-12-03 23:15:27 +0530

    xl added

---
+++ Intro const & let 23-11/ConstVariable.js
@@ -0,0 +1,13 @@
+// The const keyword > cannot be Redeclared
+//                   > cannot be Reassigned
+//                   > have Block Scope
+
+
+// Example
+const PI_1 = 3.1415;
+// PI = 3.14;           // const variable cannot be reassigned
+
+
+// const PI_2;             // const variables must be assigned a value when they are declared
+// PI_2 = 3.14159265359;
+

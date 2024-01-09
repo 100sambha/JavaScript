84fdf2d47a3814670ab537f5d9f6f9291ace8ccf
100sambha <100sambha@gmail.com>
2024-01-09 22:32:34 +0530

    JSON Concepts are completed

---
+++ MileStone1/2 December/08-12 JSON/JSONvsXML.js
@@ -0,0 +1,36 @@
+{"employees"=[
+    { "firstName":"John", "lastName":"Doe" },
+    { "firstName":"Anna", "lastName":"Smith" },
+    { "firstName":"Peter", "lastName":"Jones" }
+]}
+
+<employees>
+  <employee>
+    <firstName>John</firstName> <lastName>Doe</lastName>
+  </employee>
+  <employee>
+    <firstName>Anna</firstName> <lastName>Smith</lastName>
+  </employee>
+  <employee>
+    <firstName>Peter</firstName> <lastName>Jones</lastName>
+  </employee>
+</employees>
+
+
+// Using XML
+
+// Fetch an XML document
+// Use the XML DOM to loop through the document
+// Extract values and store in variables
+
+
+// Using JSON
+
+// Fetch a JSON string
+// JSON.Parse the JSON string
+// JSON doesn't use end tag
+// JSON is shorter
+// JSON is quicker to read and write
+// JSON can use arrays
+
+

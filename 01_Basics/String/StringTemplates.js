c8b8ac4bd3da23e6afc0de6ed692064affd4c879
100sambha <100sambha@gmail.com>
2024-08-05 17:30:02 +0530

    String Templates done

---
+++ 01_Basics/String/StringTemplates.js
@@ -0,0 +1,20 @@
+let text = `Hello Sambhaji`;
+console.log(text);
+
+text = `Lorem Ipsum is simply dummy text 
+of the printing and typesetting industry. 
+Lorem Ipsum has been the industry's standard 
+dummy text ever since the 1500s`;
+
+console.log(text);
+
+let firstName = "Sambhaji";
+let lastName = "Pandhare";
+
+let nameV = `Hello ${firstName} ${lastName}`;
+console.log(nameV);
+
+let myArr = firstName.split("");
+for(const x of myArr){
+    console.log(x);
+}

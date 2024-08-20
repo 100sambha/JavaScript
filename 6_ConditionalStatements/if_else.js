f516d1a4b8e196e8ab590df0ad31e5f58314a775
100sambha <100sambha@gmail.com>
2024-08-20 16:27:43 +0530

    control statement done

---
+++ 6_ConditionalStatements/if_else.js
@@ -0,0 +1,13 @@
+const bookPrice = 500;
+if(bookPrice==100){
+    console.log("Book Price is :"+bookPrice)
+}
+else if(bookPrice>100){
+    console.log("Book Price is greater than 100");
+}
+else if(bookPrice<100){
+    console.log("Book price is less than 100");
+}
+else{
+    console.log("Something wrong");
+}
---
+++ 6_ConditionalStatements/switchCase.js
@@ -0,0 +1,19 @@
+let num = 2;
+switch(num){
+    case 1:console.log("MonDay");
+    break;
+    case 2:console.log("Tuesday");
+    break;
+    case 3:console.log("Wedensday");
+    break;
+    case 4:console.log("Thursday");
+    break;
+    case 5:console.log("Friday");
+    break;
+    case 6:console.log("Saturday");
+    break;
+    case 7:console.log("Sunday");
+    break;
+    default:console.log("Invalid Input");
+    break;
+}

283fe994dde62a5bb084272bde6dbc3a8dcad0d7
100sambha <100sambha@gmail.com>
2024-08-20 16:31:05 +0530

    control statement done2

--- 6_ConditionalStatements/switchCase.js
+++ 6_ConditionalStatements/switchCase.js
@@ -1,17 +1,24 @@
 let num = 2;
 switch(num){
+    case "one":
     case 1:console.log("MonDay");
     break;
+    case "two":
     case 2:console.log("Tuesday");
     break;
+    case "three":
     case 3:console.log("Wedensday");
     break;
+    case "four":
     case 4:console.log("Thursday");
     break;
+    case "five":
     case 5:console.log("Friday");
     break;
+    case "six":
     case 6:console.log("Saturday");
     break;
+    case "seven":
     case 7:console.log("Sunday");
     break;
     default:console.log("Invalid Input");
